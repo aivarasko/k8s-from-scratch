bd220bc652b16710f6c3d9157eeabcd8c4a0df0b
Aivaras Ko <aivaras.kondrotas@gmail.com>
2020-08-09 07:27:42 +0300

    Bump versions

--- versions.sh
+++ versions.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-export GO_VERSION='1.14.3'
-export CFSSL_GIT_VERSION='1.3.4'
+export GO_VERSION='1.14.7'
+export CFSSL_GIT_VERSION='1.4.1'
 export CNI_GIT_VERSION='v0.8.2'
-export CONTAINERD_GIT_VERSION='v1.3.0'
-export ETCD_GIT_VERSION='v3.4.3'
+export CONTAINERD_GIT_VERSION='v1.3.7'
+export ETCD_GIT_VERSION='v3.4.10'
 export KUBERNETES_GIT_VERSION='v1.16.13'
 export SONOBUOY_VERSION='0.18.4'
 export CRICTL_VERSION='v1.16.1'
-export RUNC_GIT_VERSION='v1.0.0-rc9'
+export RUNC_GIT_VERSION='v1.0.0-rc92'
 export S3_CACHE_BUCKET='default-k8s-cache-bucket'
